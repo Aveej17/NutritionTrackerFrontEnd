09b32fd44e5dde944b9312a4524451653beec594
Aveej17 <jeevaboobathi1718@gmail.com>
2025-12-21 13:41:10 +0530

    Default login page

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from '@/pages/Login';
 import Signup from '@/pages/Signup';
 import Index from '@/pages/Index';
@@ -8,6 +8,7 @@ import Upgrade from '@/pages/Upgrade';
 export default function AppRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/app" element={<Index />} />
